ff3ed94cc0f5f8496ddd270414dfbc38765c0c30
Juan Evangelista Fletes Garcia <juanfletes007@gmail.com>
2022-10-30 14:37:40 -0700

    tarea#3 Agregando validaciones al modelo

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  validates :title, :description, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.0 }
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+      with:
+      %r{\.(gif|jpg|png)\z}i,
+      message: 'must be a URL for GIF, JPG or PNG image.'
+      }
 end
