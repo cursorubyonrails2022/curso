306c855f595f1cb6adfdd9e7acfd03b72bc3e33c
Juan Evangelista Fletes Garcia <juanfletes007@gmail.com>
2022-10-30 13:04:18 -0700

    Agregando bootstrap

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(
+  title: 'Galleta Oreo',
+  description: 'Galletas Oreo se caracteriza por estar constituida por dos galletas de chocolate circulares unidas por una <b>crema</b>',
+  image_url: 'https://miro.medium.com/max/720/0*APFGTr34fZr4YPvb.jpg',
+  price: 6.0
+)
+
+Product.create(
+  title: 'Galleta salvado y miel',
+  description: '¡Cuídate como te gusta con galletas salvado y miel TOSH!',
+  image_url: 'https://tosh.com.co/wp-content/uploads/2020/07/galletas-miel-tosh-300x194.png',
+  price: 8.0
+)
